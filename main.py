8f3475b92831a09d27c21c1113de91b339f760e3
Marat Zaripov <maratos96@gmail.com>
2023-12-12 20:27:33 +0300

    данные

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from settings import OPERATIONS_PATH
+from utils import get_all_operations, get_operations_instances
+
+
 def main():
-    pass
+    all_operations = get_all_operations(OPERATIONS_PATH)
+    operation_instances = get_operations_instances(all_operations)
+    print()
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
 
+from models.operation import Operation
+
 
 def get_all_operations(path) -> list[dict]:
     """
@@ -9,3 +11,21 @@ def get_all_operations(path) -> list[dict]:
     """
     with open(path, encoding='utf8') as file:
         return json.load(file)
+
+
+def get_operations_instances(operations: list[dict]):
+    operation_instances = []
+    for operation in operations:
+        if operation:
+            operation_instance = Operation(
+                pk=operation["id"],
+                date=operation["date"],
+                state=operation["state"],
+                operation_amount=operation["operationAmount"],
+                description=operation["description"],
+                from_=operation.get("from", ""),
+                to=operation["to"]
+
+            )
+            operation_instances.append(operation_instance)
+    return operation_instances
