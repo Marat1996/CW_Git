63824635c7e582642f0dc88414181ffbbc721476
Marat Zaripov <maratos96@gmail.com>
2023-12-13 20:01:03 +0300

    функции добавлены

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from utils import get_all_operations, get_operations_instances
 def main():
     all_operations = get_all_operations(OPERATIONS_PATH)
     operation_instances = get_operations_instances(all_operations)
-    print()
+
+    # Выводим информацию о последних 5 операциях
+    for operation_instance in operation_instances[:5]:
+        formatted_operation = operation_instance.format_operation()
+        print(formatted_operation)
+        print()  # Пустая строка между операциями
 
 
 if __name__ == '__main__':
--- models/operation.py
+++ models/operation.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Operation:
     def __init__(
             self,
@@ -17,3 +20,43 @@ class Operation:
         self.description = description
         self.from_ = from_
         self.to = to
+
+    def format_operation(self) -> str:
+        formatted_date = datetime.strptime(self.date, "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
+        masked_from = self.mask_account_number(self.from_)
+        masked_to = self.mask_account_number(self.to)
+
+        formatted_operation = f"{formatted_date} {self.description}\n" \
+                              f"{masked_from} -> {masked_to}\n" \
+                              f"{self.operation_amount['amount']} {self.operation_amount['currency']['name']}\n"
+        return formatted_operation
+
+    def mask_account_number(self, account_number: str) -> str:
+        if account_number.startswith("Счет"):
+            # Маскировка первых двух цифр счета
+            return f"Счет {'*' * (len(account_number) - 4)}{account_number[-2:]}"
+        elif account_number.startswith(("Maestro", "Master", "Visa")):
+            # Разбиваем номер карты на части
+            card_parts = account_number.split()
+            # Маскировка номера карты
+            masked_card_number = f"{card_parts[0]} {'*' * 4} {'*' * 4} {card_parts[1][-4:]}"
+            return f"{masked_card_number}"
+        else:
+            # Другие случаи, просто маскируем середину
+            return f"{account_number[:6]}{'*' * (len(account_number) - 12)}{account_number[-4:]}"
+
+    def convert_payment(self) -> str:
+        if self.description.startswith("Перевод организации"):
+            masked_from = self.mask_account_number(self.from_)
+            masked_to = self.mask_account_number(self.to)
+            return f"{masked_from} -> {masked_to}\n" \
+                   f"{self.operation_amount['amount']} {self.operation_amount['currency']['name']}"
+        elif self.description.startswith("Открытие вклада"):
+            return f"Открытие вклада\n" \
+                   f" -> {self.mask_account_number(self.to)}\n" \
+                   f"{self.operation_amount['amount']} {self.operation_amount['currency']['name']}"
+        # Добавьте другие условия для различных видов платежей, если необходимо
+        else:
+            return f"{self.description}\n" \
+                   f"{self.mask_account_number(self.from_)} -> {self.mask_account_number(self.to)}\n" \
+                   f"{self.operation_amount['amount']} {self.operation_amount['currency']['name']}"
