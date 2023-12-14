9753cb40b9d128b96e879f8da68dbe69a9a3c897
Marat Zaripov <maratos96@gmail.com>
2023-12-14 16:58:42 +0300

    сортировка по дате

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from settings import OPERATIONS_PATH
 from utils import get_all_operations, get_operations_instances
 
@@ -6,11 +8,13 @@ def main():
     all_operations = get_all_operations(OPERATIONS_PATH)
     operation_instances = get_operations_instances(all_operations)
 
-    for operation_instance in operation_instances[:5]:
+
+    sorted_operations = sorted(operation_instances, key=lambda op: datetime.strptime(op.date, "%Y-%m-%dT%H:%M:%S.%f"))
+
+    for operation_instance in sorted_operations[:5]:
         formatted_operation = operation_instance.format_operation()
         print(formatted_operation)
         print()
-
 
 if __name__ == '__main__':
     main()
